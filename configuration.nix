f6cb02237bbaf4acde99471e910fe3bef4d0e1cf
David Olafsson <d@olafsson.ch>
2023-12-07 17:30:20 +0100

    add kernelpackages latest

--- configuration.nix
+++ configuration.nix
@@ -21,7 +21,7 @@ nix.settings.experimental-features = [ "nix-command" "flakes" ];
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
+  boot.kernelPackages = pkgs.linuxPackages_latest; 
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
@@ -191,6 +191,7 @@ fonts.packages = with pkgs; [
   libsForQt5.xdg-desktop-portal-kde # for KDE styles in firefox
   tree
   pciutils
+  
   ];
 
   environment.variables.EDITOR = "emacs"; 
